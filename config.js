62194d571beb9e80fc15292115e76576815ccbc5
sterling <102415551+lzh-zone@users.noreply.github.com>
2023-02-18 09:35:58 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,56 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Sterling Websites',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2023191-23f1e22f0b23309a333fb93f',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://home.zihao.ml/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.lzhnb.cf/'
+    },
+    {
+      text: '音乐',
+      url: 'https://music.zihao.ml/'
+    },
+    {
+      text: '影视1',
+      url: 'https://sterling.cf/'
+    }
+    {
+      text: '影视2',
+      url: 'https://lzhin.totalh.net/'
+    }
+    {
+      text: '影视3',
+      url: 'https://jx.lzhtv.cf/'
+    }
+    {
+      text: '解析',
+      url: 'https://zihao.ml/'
+    }
+    {
+      text: '盒子',
+      url: 'https://box.zihao.ml/'
+    }
+  ],
+};
